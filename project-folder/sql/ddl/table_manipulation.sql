9c24d1aa7dc9fb153bf322baac7f1019d60db74d
nsk-here <nishantnayan2002@gmail.com>
2024-04-14 23:13:39 +0530

    Data cleaning

--- project-folder/sql/ddl/table_manipulation.sql
+++ project-folder/sql/ddl/table_manipulation.sql
@@ -2,11 +2,13 @@ USE project_db;
 /*
 1. Datatype Format, typeCast it
 2. Check constraint
+*/
 --To find the primary key constraint
 SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE
 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
-WHERE TABLE_NAME='genome_tag';
-*/
+WHERE TABLE_NAME='tags';
+
+
 
 --movieLens.movies
 select
@@ -154,6 +156,122 @@ ALTER COLUMN tmdbId INT NOT NULL;
 
 ALTER TABLE movieLens.links
 ADD CONSTRAINT pk_links PRIMARY KEY(movieId, imdbId, tmdbId);
+
+-- movieLens.rating
+SELECT
+	TABLE_NAME,
+	COLUMN_NAME,
+	DATA_TYPE,
+	CHARACTER_MAXIMUM_LENGTH
+FROM information_schema.columns 
+WHERE table_name = 'rating'
+
+-- check the duplicacy
+SELECT userId, movieId, COUNT(*) AS num_duplicates
+FROM movieLens.rating
+GROUP BY userId, movieId
+HAVING COUNT(*) > 1;
+
+-- check null
+select * from 
+movielens.rating
+where movieId is NULL;
+
+-- apply primary key
+ALTER TABLE movielens.rating
+ALTER COLUMN userId INT NOT NULL;
+
+ALTER TABLE movielens.rating
+ALTER COLUMN movieId INT NOT NULL;
+
+ALTER TABLE movielens.rating
+ADD CONSTRAINT pk_ratings PRIMARY KEY (userId, movieId);
+
+
+-- movieLens.tags
+SELECT
+	TABLE_NAME,
+	COLUMN_NAME,
+	DATA_TYPE,
+	CHARACTER_MAXIMUM_LENGTH
+FROM information_schema.columns 
+WHERE table_name = 'tags'
+
+-- formatting column datatype: in sqlserver altering the column, only one column in one command
+ALTER TABLE movieLens.tags
+ALTER COLUMN userId INT;
+
+ALTER TABLE movieLens.tags
+ALTER COLUMN movieId INT;
+
+UPDATE movieLens.tags
+set timestamp = DATEADD(second, CAST(timestamp as BIGINT), '1970-01-01');
+ALTER TABLE movieLens.tags
+ALTER COLUMN timestamp DATE;
+
+select top 5 * from movielens.tags 
+-- Assuming the timestamp value is stored in a column named 'timestamp_column' in a table named 'your_table_name'
+SELECT DATEADD(second, 1439472355, '1970-01-01') AS readable_timestamp;
+
+UPDATE movielens.tags
+set tag = 'unknown tag'
+where tag like '??%';
+
+
+SELECT 
+	userid,
+	movieid,
+	tag,
+	count(*)
+from movieLens.tags
+group by userid, movieId, tag
+having count(*)>1;
+
+SELECT
+	userid,
+	movieid,
+	tag
+from movieLens.tags
+where tag like '??%';
+
+WITH duplicates_cte as(
+select
+	userId,
+	movieId,
+	tag,
+	ROW_NUMBER()over(partition by userId, movieId, tag order by movieId) as rnk
+from movieLens.tags)
+delete from duplicates_cte
+where rnk > 1;
+
+SELECT
+	*
+from movieLens.tags
+where tag is NULL;
+
+--set column as null;
+ALTER table movieLens.tags
+ALTER COLUMN userId INT NOT NULL;
+
+ALTER table movieLens.tags
+ALTER COLUMN movieId INT NOT NULL;
+
+ALTER table movieLens.tags
+ALTER COLUMN tag varchar(200) NOT NULL;
+
+ALTER table movieLens.tags
+ADD CONSTRAINT pk_tags PRIMARY KEY(userId, movieId, tag);
+
+-- with this DATA cleaning completed
+
+
+
+
+
+
+
+
+
 
 
 

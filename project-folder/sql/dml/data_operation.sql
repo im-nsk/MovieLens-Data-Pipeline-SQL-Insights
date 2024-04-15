55662d0873a2ebd45f215f69c552930c221b6ef6
nsk-here <nishantnayan2002@gmail.com>
2024-04-15 22:09:17 +0530

    Create data_operation.sql

---
+++ project-folder/sql/dml/data_operation.sql
@@ -0,0 +1,45 @@
+/*Join Tables: You can join the tables to combine relevant information. For example, joining the "movies" table with the "ratings" table on the "movieId" column can give you ratings for each movie. Similarly, joining "movies" with "tags" can give you user-generated tags for each movie.
+*/
+--1. Calculate Average Ratings: With the joined table of movies and ratings, we can calculate the average rating for each movie. 
+
+--2. Count Number of Ratings: We can count the number of ratings each movie has received. 
+
+--3. List Most Common Tags: After joining the movies table with the tags table, we can list the most common tags associated with movies. 
+
+-- 4. Explore Genre-Rating Relationship: We can analyze the relationship between movie genres and ratings by joining the movies table with the ratings table and grouping by genres.
+
+/*
+Aggregation: Calculate statistics such as average ratings, count of ratings, or average relevance of tags for each movie.*/
+--1 Average Relevance of Tags for Each Movie:
+
+/*Exploratory Data Analysis (EDA): Explore the data distribution, identify trends, and understand the characteristics of movies, ratings, tags, etc.*/
+-- 1. Distribution of Ratings:
+
+-- 2. Distribution of Genres:
+
+-- 3. Trend of movie Over Time
+
+-- 4. Number of Movies per Year 
+
+-- 5. Average Ratings per Genre:
+
+
+/*Genre Analysis: Analyze the distribution of genres among movies. You can count the occurrences of each genre and visualize them.*/
+
+-- 1. Count Occurrences of Each Genre:
+--Visualize the Distribution of Genres:You can use Power BI
+
+/*Collaborative Filtering: Use collaborative filtering techniques to recommend movies based on user ratings.*/
+-- collaborative filtering to recommend movies based on user ratings:
+
+/*Content-Based Filtering: Analyze movie tags and genres to recommend similar movies based on content.*/
+-- content-based filtering using movie tags and genres:
+
+
+/*Dimensionality Reduction: Use techniques like Principal Component Analysis (PCA) on the genome scores to reduce dimensionality and find latent features.*/
+
+
+/*Data Visualization: Visualize insights using Power BI or any other data visualization tool. Create charts, graphs, and dashboards to represent the findings effectively.*
+
+Machine Learning Models: Build predictive models for movie ratings or user preferences using techniques like regression, classification, or clustering.
+--coming soon

d52c22f52ae1fe6eb8eac0e7296e06ff0c820cad
nsk-here <nishantnayan2002@gmail.com>
2024-04-18 09:44:33 +0530

    Update foreign key

--- project-folder/sql/ddl/foreign_constraint.sql
+++ project-folder/sql/ddl/foreign_constraint.sql
@@ -1,14 +1,53 @@
-ALTER TABLE movies
-ADD CONSTRAINT FK_movies_genreId FOREIGN KEY (genreId) REFERENCES genres(genreId);
+use project_db;
+--Links Table:
+--Verify Data Consistency
+select m.movieId, l.movieId
+from movieLens.movies m RIGHT JOIN movieLens.links l on m.movieId = l.movieId
+where m.movieId is NULL;
+--Correct Data: 
+DELETE FROM movieLens.links
+where movieId NOT IN(select movieId from movieLens.movies);
+-- Add Foreign Key Constraint: 
+ALTER TABLE movielens.links
+ADD CONSTRAINT FK_links_movieId FOREIGN KEY (movieId) REFERENCES movielens.movies(movieId);
 
-ALTER TABLE links
-ADD CONSTRAINT FK_links_imdbId FOREIGN KEY (imdbId) REFERENCES imdb(imdbId);
+--Genome Scores Table:
+--Verify Data Consistency
+select m.movieId, gm.movieId
+from movieLens.movies m RIGHT JOIN movieLens.genome_scores gm on m.movieId = gm.movieId
+where m.movieId is NULL;
+-- Add Foreign Key Constraint: 
+ALTER TABLE movielens.genome_scores
+ADD CONSTRAINT FK_genome_scores_movieId FOREIGN KEY (movieId) REFERENCES movielens.movies(movieId);
 
-ALTER TABLE genome_scores
-ADD CONSTRAINT FK_genome_scores_tagId FOREIGN KEY (tagId) REFERENCES genome_tags(tagId);
+--Verify Data Consistency
+select gt.tagId, gm.tagId
+from movieLens.genome_tags gt RIGHT JOIN movieLens.genome_scores gm on gt.tagId = gm.tagId
+where gt.tagId is NULL;
+-- Add Foreign Key Constraint: 
+ALTER TABLE movieLens.genome_scores
+ADD CONSTRAINT FK_genome_scores_tagId FOREIGN KEY (tagId) REFERENCES movieLens.genome_tags(tagId);
 
-ALTER TABLE tags
-ADD CONSTRAINT FK_tags_userId FOREIGN KEY (userId) REFERENCES users(userId);
+--Tags Table:
+--Verify Data Consistency
+select m.movieId, t.movieId
+from movieLens.movies m RIGHT JOIN movieLens.tags t on m.movieId = t.movieId
+where m.movieId is NULL;
+-- Correct Data:
+DELETE FROM movieLens.tags
+where movieId NOT IN(select movieId from movieLens.movies);
+-- Add Foreign Key Constraint: 
+ALTER TABLE movieLens.tags
+ADD CONSTRAINT FK_tags_movieId FOREIGN KEY (movieId) REFERENCES movieLens.movies(movieId);
 
-ALTER TABLE ratings
-ADD CONSTRAINT FK_ratings_userId FOREIGN KEY (userId) REFERENCES users(userId);
+--Ratings Table:
+select m.movieId, r.movieId
+from movieLens.rating r LEFT JOIN movieLens.movies m on r.movieId = m.movieId
+where m.movieId IS NULL;
+--Correct data
+DELETE FROM movieLens.rating
+WHERE movieId NOT IN (SELECT movieId FROM movieLens.movies);
+-- Add Foreign Key Constraint:
+ALTER TABLE movieLens.rating
+ADD CONSTRAINT FK_ratings_movieId FOREIGN KEY (movieId) REFERENCES movieLens.movies(movieId);
+
--- project-folder/sql/ddl/index_creation.sql
+++ project-folder/sql/ddl/index_creation.sql
@@ -1,16 +1,18 @@
-CREATE INDEX IX_movies_title ON movies(title);
-CREATE INDEX IX_movies_genres ON movies(genres);
+USE project_db;
 
-CREATE INDEX IX_links_movieId ON links (movieId);
+CREATE INDEX IX_movies_title ON movieLens.movies(title);
+CREATE INDEX IX_movies_genres ON movieLens.movies(genres);
 
-CREATE INDEX IX_genome_tags_tag ON genome_tags (tag);
+CREATE INDEX IX_links_movieId ON movieLens.links (movieId);
 
-CREATE INDEX IX_genome_scores_movieId ON genome_scores (movieId);
-CREATE INDEX IX_genome_scores_tagId ON genome_scores (tagId);
+CREATE INDEX IX_genome_tags_tag ON movieLens.genome_tags (tag);
 
-CREATE INDEX IX_tags_userId ON tags (userId);
-CREATE INDEX IX_tags_movieId ON tags (movieId);
+CREATE INDEX IX_genome_scores_movieId ON movieLens.genome_scores (movieId);
+CREATE INDEX IX_genome_scores_tagId ON movieLens.genome_scores (tagId);
 
-CREATE INDEX IX_ratings_userId ON ratings (userId);
-CREATE INDEX IX_ratings_movieId ON ratings (movieId);
+CREATE INDEX IX_tags_userId ON movieLens.tags (userId);
+CREATE INDEX IX_tags_movieId ON movieLens.tags (movieId);
+
+CREATE INDEX IX_ratings_userId ON movieLens.rating (userId);
+CREATE INDEX IX_ratings_movieId ON movieLens.rating (movieId);
 
